f5264ebef9930de2d39d36099532aa12f10ff87f
Andri30032001 <andrymone30032001@gmail.com>
2021-04-27 04:52:06 +0700

    project pertama

---
+++ application/controllers/Coba.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ */
+class Coba extends CI_Controller {
+	
+	public function index () {
+		$this->load->view('coba1');
+	}
+}
+?>
---
+++ application/controllers/users.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->model('users_model');
+	}
+
+	public function index(){
+		$data['users'] = $this->users_model->getAllUsers();
+		$this->load->view('user_list.php', $data);
+	}
+}
+
+?>
